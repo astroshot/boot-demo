93e7f3bcd794d04d84252b36033cc26965c7348b
liuzhao11 <liuzhao11@baidu.com>
2019-04-28 17:30:41 +0800

    fix db config

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE `user` (
   `email` varchar(64) NOT NULL DEFAULT '' COMMENT '邮箱',
   `phone` varchar(32) NOT NULL DEFAULT '' COMMENT '手机号',
   `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态0|1|9:初始添加|正常|注销',
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_unique` (`email`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
