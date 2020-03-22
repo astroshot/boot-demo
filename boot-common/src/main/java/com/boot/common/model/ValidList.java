c799f3ef249294086b7025ab5435eda59b8db5e7
Shockwave <astroshot@outlook.com>
2020-03-22 09:41:04 +0800

    update validlist

--- boot-common/src/main/java/com/boot/common/model/ValidList.java
+++ boot-common/src/main/java/com/boot/common/model/ValidList.java
@@ -3,9 +3,9 @@ package com.boot.common.model;
 import lombok.Data;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -13,7 +13,7 @@ import java.util.ListIterator;
 public class ValidList<E> implements List<E> {
 
     @Valid
-    private List<E> list = new LinkedList<>();
+    private List<E> list = new ArrayList<>();
 
     @Override
     public int size() {
@@ -51,8 +51,18 @@ public class ValidList<E> implements List<E> {
     }
 
     @Override
+    public void add(int index, E element) {
+        list.add(index, element);
+    }
+
+    @Override
     public boolean remove(Object o) {
         return list.remove(o);
+    }
+
+    @Override
+    public E remove(int index) {
+        return list.remove(index);
     }
 
     @Override
@@ -93,16 +103,6 @@ public class ValidList<E> implements List<E> {
     @Override
     public E set(int index, E element) {
         return list.set(index, element);
-    }
-
-    @Override
-    public void add(int index, E element) {
-        list.add(index, element);
-    }
-
-    @Override
-    public E remove(int index) {
-        return list.remove(index);
     }
 
     @Override
